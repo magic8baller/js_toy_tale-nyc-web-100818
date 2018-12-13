f20a8bcf40aba28fd3177e484b90c1811528b8df
magic8baller <lturis@gmail.com>
2018-12-13 06:39:36 -0500

    redonez

--- src/index.js
+++ src/index.js
@@ -1,80 +1,103 @@
-
-
-// VARIABLES
 const addBtn = document.querySelector('#new-toy-btn')
-const toyForm = document.querySelector('.container')
 const toyCollection = document.querySelector('#toy-collection')
+const toyForm = document.querySelector('.container')
+const url = 'http://localhost:3000/toys'
 let addToy = false
-const toysUrl = 'http://localhost:3000/toys'
-let newToy = {
-  name: '',
-  image: '',
-  likes: 0
-}
-let likesBtn = document.querySelector('button')
+let allToys = []
 
-
-const toyTemplate = (toy) => {
-  toyCollection.innerHTML += `
-  <div class='card'>
-    <h3>${toy.name}</h3>
-    <img src='${toy.image}' class='toy-avatar'>
-    <p>${toy.likes} Likes</p>
-    <button class='like-btn'>Like <3</button>
-    <button class='like-btn delete' data-id='${toy.id}'>NO THX --</button>
-  </div>
-  `
-
-
-}
-
-
-// EVENT LISTENERS
-
-//2- update dom - dom is ready
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('help me i am trapped in this loaded web browser!')
-  // render
-  getToys(toysUrl).then(json => json.map(toyTemplate))
+  console.log('%c help me im trapped in a loaded browzer!', 'color: yellow')
+
+  fetch(url)
+    .then(resp => resp.json())
+    .then(json => {
+      allToys = json
+      renderAll(allToys)
+    })
 })
 
-// form behavior - can target scope
+
 addBtn.addEventListener('click', () => {
-  // hide & seek with the form
   addToy = !addToy
   if (addToy) {
-    toyForm.style.display = 'block'
-    //target form scope- has own event listener for SUBMIT!
-    // submit listener
-    const form = toyForm.querySelector('.add-toy-form')
-    form.addEventListener('submit',(e) => {
-      e.preventDefault()
-      //update newToy object
-      newToy.name = form.querySelector("input[name='name']").value
-      newToy.image = form.querySelector("input[name='image']").value
-      // submit and add to DB
-      addToyToDb(toysUrl, newToy)
-      // .then(data => console.log(data))
-})
+    toyForm.style.display = 'block';
   } else {
-    toyForm.style.display = 'none'
+    toyForm.style.display = 'none';
   }
+});
+
+function toyTemplate(toy) {
+  toyCollection.innerHTML += `
+    <div class='card' data-id='${toy.id}'>
+      <h2>${toy.name}</h2>
+      <img src='${toy.image}' class='toy-avatar'>
+      <p><span class='toy-likes'>${toy.likes}</span> Likes</p>
+      <button class='like-btn likes' data-id='${toy.id}' data-action='like'>Like <3</button>
+      <br>
+      <button class='like-btn' data-id='${toy.id}' data-action='delete'>DESTROY THIS SHIT</button>
+    </div>
+    `
+}
+
+function renderAll(toys) {
+  toys.map(toyTemplate)
+}
+
+function postNewToy(options) {
+  fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(options)
+    })
+    .then(res => res.json())
+    .then(toyTemplate)
+}
+
+toyForm.addEventListener('submit', e => {
+  e.preventDefault()
+  let name = e.target.name.value
+  let image = e.target.image.value
+
+  const data = {
+    "name": name,
+    "image": image,
+    "likes": 0
+  }
+  postNewToy(data)
 })
 
-// likesBtn.addEventListener('click', (e) => {
-//   let toyId = e.target.dataset.id
+function updateLikes(id, likes) {
+  fetch(`${url}/${id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify({
+      likes
+    })
+  })
+}
 
-// })
+toyCollection.addEventListener('click', e => {
 
-// const findLikes = (toyId) => {
-//   let currentToy = 
-// }
+  if (e.target.className === 'like-btn') {
+    let toyId = e.target.dataset.id
+    let toyLikes = e.target.parentElement.querySelector('.toy-likes')
 
-// function requestData() {
-  // return fetch(toyUrl).then(res => res.json())
+    if (e.target.dataset.action === 'like') {
+      toyLikes.textContent = parseInt(toyLikes.textContent) + 1
+      let incrementLikes = toyLikes.textContent
+      updateLikes(toyId, incrementLikes)
 
-// ./.DOMContentLoaded.. (e) =>
-//     requestData().then(json => {
-  // json.map(toyTemplate) 
-// does render all for u basically?
-
+    } else if (e.target.dataset.action === 'delete') {
+      e.target.parentElement.remove()
+      fetch(`${url}/${toyId}`, {
+        method: 'DELETE'
+      })
+    }
+  }
+});
